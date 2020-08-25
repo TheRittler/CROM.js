1053edf7e07e900c95e630a62b4b3715221e628e
redalertexpert <felix.rittler@outlook.com>
2020-08-25 20:01:55 +0200

    Add enums to index to make them usable

--- index.ts
+++ index.ts
@@ -1,2 +1,8 @@
-import { Crom_l1_composedFactoryImpl } from 'crom_l1_composed/Crom_l1_composedFactoryImpl';
-export { Crom_l1_composedFactoryImpl }
+import { Crom_l1_composedFactoryImpl } from "crom_l1_composed/Crom_l1_composedFactoryImpl";
+import { Parthood } from "./crom/Parthood";
+import { Direction } from "crom_l1_composed/Direction";
+
+export { Crom_l1_composedFactoryImpl };
+
+export { Parthood };
+export { Direction };
